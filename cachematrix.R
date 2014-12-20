c1b2dd4f2083010f9087161fd2b9b6af306f6294
jackbrzezinski <jackbrzezinski@gmail.com>
2014-12-20 00:45:58 -0500

    cachematrix file edited

--- cachematrix.R
+++ cachematrix.R
@@ -1,14 +1,21 @@
-## Put comments here that give an overall description of what your
-## functions do
+## These 2 functions allow the inverse of a matrix to be "cached"
+## meaning that it is stored in the global environment and can be
+## accessed even if the function in which the variable with the
+## value assigned has already been called.
 
-## Write a short comment describing this function
+## This function creates a list of functions for a matrix which
+## caches the value of "slv" --> the variable that will hold the
+## solution to the inverse matrix. With this function the initial
+## value of "slv" is null.
 
 makeCacheMatrix <- function(x = matrix()) {
 
 }
 
 
-## Write a short comment describing this function
+## This function calculates the inverse of the matrix passed as the
+## arguement for makeCacheMatrix and stores the value in "slv". This
+## value is cached in the global environment.
 
 cacheSolve <- function(x, ...) {
         ## Return a matrix that is the inverse of 'x'
